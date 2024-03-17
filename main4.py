7cb889c7f54dc53e0db197ce57010052a6d11c2d
viperfish-oc <71723888+viperfish-oc@users.noreply.github.com>
2024-03-17 19:58:20 +0100

    rework to OOP, fixed forces, move plotting to external file

---
+++ main4.py
@@ -0,0 +1,117 @@
+import numpy as np
+from visplot import *
+
+class Box:
+    def __init__(self, particle_positions, particle_velocities, box_length):
+        self.positions = particle_positions
+        self.velocities = particle_velocities
+        self.box_length = box_length
+        self.n_particles = np.shape(particle_positions)[0]
+        self.n_dimensions= np.shape(particle_positions)[1]
+        
+    def step_forward_euler(self, h):
+        self.positions = self.positions + h*self.velocities
+        self.velocities = self.velocities + h*self.get_forces()
+        return
+
+        
+    def get_connecting_vectors(self):
+        diff_matrix = np.empty((self.n_dimensions, self.n_particles, self.n_particles))
+        for dim in range(0, self.n_dimensions):
+            axis_positions = self.positions[:, dim]
+            c1 = np.repeat(axis_positions[:, np.newaxis], self.n_particles, axis=1)
+            c2 = np.repeat(axis_positions[np.newaxis, :], self.n_particles, axis=0)
+            diff_matrix[dim, :, :] = c1 - c2
+
+        return diff_matrix
+
+    def normalize_connecting_vectors(self, diff_matrix):
+
+        # sum the squares of elements across all dimensions
+        diff_matrix_norm = np.empty((self.n_particles, self.n_particles))
+        norm = np.linalg.norm(diff_matrix, axis=0)
+        diff_matrix_norm = norm
+
+        # do the normalization
+        dim_tiled_norm = np.repeat(diff_matrix_norm[np.newaxis, :, :], self.n_dimensions, axis=0)
+        diff_matrix = diff_matrix / dim_tiled_norm
+
+        return diff_matrix
+
+    def get_radial_distances(self, diff_matrix):
+        radial_distances = np.linalg.norm(diff_matrix, axis=0) # sum over dimensions
+        return radial_distances
+
+    def get_force_magnitudes(self, radial_distances):
+        forces_magnitudes = 4 * (-12 * radial_distances**-13 + 6 * radial_distances**-7)
+        return forces_magnitudes
+
+
+    def get_forces(self):
+        connecting_vectors = self.get_connecting_vectors() 
+        radial_distances = self.get_radial_distances(connecting_vectors)
+        force_magnitudes = self.get_force_magnitudes(radial_distances) 
+        connecting_vectors = self.normalize_connecting_vectors(connecting_vectors)
+
+        #TODO normalization
+        force_per_particle =  connecting_vectors * (np.repeat(force_magnitudes[np.newaxis, :, :], self.n_dimensions, axis=0))
+        force_per_particle[np.isnan(force_per_particle)] = 0
+
+        forces = np.sum(force_per_particle, axis=1)
+        forces = np.transpose(forces)
+        return forces
+
+        
+class Results(object):    
+    def __init__(self) :
+        pass
+
+def get_x_component(object):
+    return object[:,:,0]
+
+def get_y_component(object):
+    return object[:,:,1]
+
+class Simulation:
+    def __init__(self, system):
+        self.system = system
+        self.n_dimensions = system.n_dimensions
+        self.n_particles = system.n_particles 
+        self.results = Results()
+        return
+
+    def run_simulation(self, h=0.1, max_time=1):
+        n_steps = int(max_time/h)
+        self.results.positions = np.empty((n_steps, self.n_particles, self.n_dimensions))
+        self.results.velocities= np.empty((n_steps, self.n_particles, self.n_dimensions))
+
+        for i in range(0, n_steps):
+            self.system.step_forward_euler(h)
+            self.results.positions[i,:,:] = self.system.positions
+            self.results.velocities[i,:,:] = self.system.velocities
+        return
+
+
+L = 20
+
+x_0 = np.array([[-0.2 * L, 0.01 * L], 
+                [0.2 * L, -0.01 * L]])
+v_0 = np.array([[0.09, -0.00], 
+                [-0.09, 0.00],
+                ])
+
+testbox = Box(x_0, v_0, L )
+sim1 = Simulation(testbox)
+sim1.run_simulation(h=0.01, max_time=100)
+print((sim1.results.positions))
+print(get_x_component(sim1.results.positions))
+
+animate_results(get_x_component(sim1.results.positions), get_y_component(sim1.results.positions), view_size=0.6*L, frame_skip_multiplier=10,     trailing_frames = 100000)
+
+# print(f'{testbox.particle_positions = }')
+# print(testbox.step_forward_euler(0.1))
+# print(f'{testbox.particle_positions = }')
+
+
+
+
---
+++ visplot.py
@@ -0,0 +1,114 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+def animate_results(input_x, input_y, view_size=10, frame_interval=10, trailing_frames=1, frame_skip_multiplier = 1):
+    fig, ax = plt.subplots()
+    ax.set_xlim([-view_size, view_size])
+    ax.set_ylim([-view_size, view_size])
+
+    n_particles = np.shape(input_x)[1]
+    n_frames = np.shape(input_x)[0] + 1
+    lines = []
+    plt.grid()
+    # set up first frame for plotting and construct all lines
+    for i in range(0, n_particles):
+        frame = 0
+        plotline = ax.plot(
+            input_x[frame, i], input_y[frame, i], marker="o", linestyle="", markersize=2
+        )
+        lines.append(plotline[0])
+
+    def update(frame):
+        frame = frame_skip_multiplier * frame
+        for i in range(0, len(lines)):
+            line = lines[i]
+            trailing_frame = max(0, frame - trailing_frames)
+            line.set_xdata(
+                input_x[trailing_frame:frame, i],
+            )
+            line.set_ydata(input_y[trailing_frame:frame, i])
+        return lines
+
+    ani = animation.FuncAnimation(
+        fig=fig,
+        func=update,
+        frames=n_frames,
+        interval=frame_interval,
+        repeat=True,
+        cache_frame_data=False,
+    )
+
+    plt.show()
+
+
+def animate_quiver(
+    positions_x,
+    positions_y,
+    vec_x,
+    vec_y,
+    view_size=10,
+    frame_interval=10,
+    arrow_scaling=1,
+):
+    # n_particles = np.shape(arrow_positions)[1]
+    n_frames = np.shape(positions_x)[0]
+    print(f"{n_frames=}")
+
+    fig2, ax = plt.subplots(1, 1)
+    quiver = ax.quiver(
+        positions_x[0, :],  # 0 for first frame
+        positions_y[0, :],
+        vec_x[0, :],
+        vec_y[0, :],
+        pivot="tail",
+        # color="r",
+    )
+
+    ax.set_xlim(-view_size, view_size)
+    ax.set_ylim(-view_size, view_size)
+
+    def update_quiver(n_frame, quiver, positions_x, positions_y, vec_x, vec_y):
+        """updates the horizontal and vertical vector components by a
+        fixed increment on each frame
+        """
+        print(f"{n_frame=}")
+        U = arrow_scaling * vec_x[n_frame, :]
+        V = arrow_scaling * vec_y[n_frame, :]
+
+        positions = np.transpose(np.vstack((positions_x[n_frame, :], positions_y[n_frame, :])))
+        print(f"{np.shape(positions)=}")
+        quiver.set_offsets(positions)
+        quiver.set_UVC(U, V)
+
+        return (quiver,)
+
+    anim = animation.FuncAnimation(
+        fig2,
+        update_quiver,
+        fargs=(quiver, positions_x, positions_y, vec_x, vec_y),
+        interval=frame_interval,
+        blit=False,
+        frames=n_frames,
+    )
+    plt.show()
+
+    return
+
+# animate_results(loop_results_x[:,:,0], loop_results_x[:,:,1], view_size=0.6*L, trailing_frames = 1000)
+# animate_quiver(
+#     loop_results_x[:, :, 0],
+#     loop_results_x[:, :, 1],
+#     selected_diff[:,0],
+#     selected_diff[:,1],
+#     arrow_scaling=1,
+#     frame_interval=1,
+# )
+# animate_quiver(
+#     loop_results_x[:, :, 0],
+#     loop_results_x[:, :, 1],
+#     loop_results_F[:,0],
+#     loop_results_F[:,1],
+#     arrow_scaling=0.01,
+#     frame_interval=1,
+# )
